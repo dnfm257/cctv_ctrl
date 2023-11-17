59623007fac62336c6e5a3e173abcb7991bdbbfa
Jang SukHwan <143377935+JSHTIRED@users.noreply.github.com>
2023-11-17 17:55:41 +0900

    Update 111702.py
    주석 수정

--- 111702.py
+++ 111702.py
@@ -5,13 +5,17 @@ from ultralytics import YOLO
 
 # Function to process each frame
 def process_frame(frame: np.ndarray, model, zones, zone_annotators, box_annotators) -> np.ndarray:
+    # 프레임에 YOLO 객체 감지 적용
     results = model(frame, imgsz=1280, verbose=False)[0]
     detections = sv.Detections.from_ultralytics(results)
-
+    #각 영역에 대해 처리
     for zone, zone_annotator, box_annotator in zip(zones, zone_annotators, box_annotators):
+        #영역을 트리거 후 검출된 것을 필터링
         mask = zone.trigger(detections=detections)
         detections_filtered = detections[mask]
+        #바운딩 박스로 프레임 주석처리
         frame = box_annotator.annotate(scene=frame, detections=detections_filtered, skip_label=True)
+        #영역으로 프레임 주석처리
         frame = zone_annotator.annotate(scene=frame)
 
     return frame
@@ -21,10 +25,11 @@ model = YOLO('yolov8s.pt')
 
 # Load video information
 VIDEO = "video.mp4"
+# 주석 색상 초기화
 colors = sv.ColorPalette.default()
 video_info = sv.VideoInfo.from_video_path(VIDEO)
 
-# Define polygons and initialize zones
+# 인식영역 정의
 polygons = [
     np.array([
         [718, 595], [927, 592], [851, 1062], [42, 1059]
@@ -33,7 +38,7 @@ polygons = [
         [987, 595], [1199, 595], [1893, 1056], [1015, 1062]
     ])
 ]
-
+#정의된 영역과 해상도로 초기화
 zones = [
     sv.PolygonZone(
         polygon=polygon,
@@ -42,15 +47,21 @@ zones = [
     for polygon in polygons
 ]
 
-# Initialize zone annotators and box annotators
+# 지정된 영역에서 나오는 박스와 텍스트설정
 zone_annotators = [
     sv.PolygonZoneAnnotator(
+        #영역 지정
         zone=zone,
+        #영역색상지정
         color=colors.by_idx(index),
+        #선의 두께
         thickness=4,
+        #텍스트 두께
         text_thickness=8,
+        #텍스트 크기
         text_scale=4
     )
+    #zones의 리스트에 따라 다른 속성을 설정함
     for index, zone in enumerate(zones)
 ]
 
@@ -74,7 +85,7 @@ while cap.isOpened():
     if not ret or frame is None:
         break
 
-    # Process the frame
+    # 프레임 처리
     processed_frame = process_frame(frame, model, zones, zone_annotators, box_annotators)
 
     # Display the processed frame
